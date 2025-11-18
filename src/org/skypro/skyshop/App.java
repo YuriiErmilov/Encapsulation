29a921939071ce8abff216d435cfc3bef207be35
Юрий <yurikbc3@gmail.com>
2025-11-18 16:12:11 +0800

    HW commit three lesson

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,38 +1,75 @@
 package org.skypro.skyshop;
+import org.skypro.skyshop.article.Article;
 import org.skypro.skyshop.product.DiscountProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
-import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.*;
 import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.search.SearchEngine;
+import org.skypro.skyshop.search.Searchable;
+
+import java.sql.SQLOutput;
 
 public class App {
     public static void main(String[] args) {
-        ProductBasket basket1 = new ProductBasket("Anton");
+        ProductBasket basket = new ProductBasket("Anton");
 
         Product hookah = new DiscountProduct("Кальян: Alpha Hookah", 12000,15);
         Product theFlask = new SimpleProduct("Колба", 1500);
         Product forceps = new FixPriceProduct("Щипцы ");
 
-        basket1.addProduct(hookah);
-        basket1.addProduct(theFlask);
-        basket1.addProduct(forceps);
+        basket.addProduct(hookah);
+        basket.addProduct(theFlask);
+        basket.addProduct(forceps);
 
-        basket1.printInfoBasket();
+        basket.printInfoBasket();
 
-        System.out.println(" Общая стоимость корзины: " + basket1.getTotalPrice());
+        SearchEngine engine = new SearchEngine(10);
 
-        System.out.println(" Ищем товар который есть в корзине: " + basket1.hasProduct("Кальян: Alpha Hookah"));
+        engine.add(hookah);
+        engine.add(theFlask);
+        engine.add(forceps);
 
-        System.out.println(" Ищем товар которого нет в корзине: " + basket1.hasProduct("Mashave"));
+        Article a1 = new Article("Топ кальянов", "Список лучших кольянов....");
+        Article a2 = new Article("Какие колбы подойдут", "Колбы подойдут к....");
+        Article a3 = new Article("Акции месяца", "Самые выгодные товары....");
 
-        basket1.cleatBasket();
+        engine.add(a1);
+        engine.add(a2);
+        engine.add(a3);
 
-        basket1.printInfoBasket();
+        System.out.println("Поиск кальян");
+        printResult(engine.search("кальян"));
 
-        System.out.println(" Общая стоимость корзины: " + basket1.getTotalPrice());
+        System.out.println("Поиск акция");
+        printResult(engine.search("акция"));
 
-        System.out.println(" Ищем товар который есть в корзине: " + basket1.hasProduct("Кальян: Alpha Hookah"));
+        System.out.println("Поиск колбы");
+        printResult(engine.search("колба"));
 
 
+        System.out.println(" Общая стоимость корзины: " + basket.getTotalPrice());
+
+        System.out.println(" Ищем товар который есть в корзине: " + basket.hasProduct("Кальян: Alpha Hookah"));
+
+        System.out.println(" Ищем товар которого нет в корзине: " + basket.hasProduct("Mashave"));
+
+        basket.cleatBasket();
+
+        basket.printInfoBasket();
+
+        System.out.println(" Общая стоимость корзины: " + basket.getTotalPrice());
+
+        System.out.println(" Ищем товар который есть в корзине: " + basket.hasProduct("Кальян: Alpha Hookah"));
+
+
+    }
+
+    private static void printResult(Searchable[] search) {
+        for (Searchable s : search) {
+            if(s != null){
+                System.out.println(s.getStringRepresentation());
+            }
+        }
     }
 }
--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,13 +1,27 @@
 package org.skypro.skyshop.product;
+import org.skypro.skyshop.search.Searchable ;
 
-public abstract class Product {
+public abstract class Product implements Searchable {
     private final String name;
 
     public Product(String name) {
         this.name = name;
     }
 
+    @Override
+    public String getSearchTerm(){
+        return name;
+    }
+    @Override
+    public String getSearchableType(){
+        return "PRODUCT";
+    }
+    @Override
     public String getName() {
+        return name;
+    }
+
+    public String getNameValue(){
         return name;
     }
 

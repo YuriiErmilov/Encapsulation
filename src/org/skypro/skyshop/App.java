5a47d70018990d4b53630946a83bd7616e641b1d
Юрий <yurikbc3@gmail.com>
2025-12-15 21:01:27 +0800

    HW fourLessonEncapsulation master

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -11,6 +11,7 @@ import org.skypro.skyshop.search.Searchable;
 import org.skypro.skyshop.exception.BestResultNotFoundException;
 
 public class App {
+
     public static void main(String[] args) {
         ProductBasket basket = new ProductBasket("Anton");
 
